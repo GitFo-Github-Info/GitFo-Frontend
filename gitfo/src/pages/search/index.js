e92207d76f68fb223a0e78511b97095894d3ae2b
youngeunkim <pkhj286@gmail.com>
2023-08-23 15:56:50 +0900

    Add : 검색창

--- gitfo/src/pages/search/index.js
+++ gitfo/src/pages/search/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./index.css";
+import Logo from "../../assets/logo.png";
+import SearchTap from "../../assets/search.png";
 
 function Search() {
-  return;
+  return (
+    <div>
+      <img id="logo" src={Logo} alt="logo" />
+      <div id="hr" />
+      <div id="search">
+        <input id="searchInput" />
+        <img id="SearchTap" src={SearchTap} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Search;
