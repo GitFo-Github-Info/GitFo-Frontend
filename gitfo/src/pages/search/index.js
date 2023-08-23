19417bf01201b193c2a04f3f9b9c6671987ceea5
youngeunkim <pkhj286@gmail.com>
2023-08-23 21:52:00 +0900

    Add : api 연결 완료

--- gitfo/src/pages/search/index.js
+++ gitfo/src/pages/search/index.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
-import Logo from "../../assets/logo.png";
 import SearchTap from "../../assets/search.png";
+import axios from "axios";
 
 function Search() {
+  const [searchInput, setSearchInput] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://gitfoback.anys.kro.kr/search/users/${searchInput}?page=1`,
+      );
+      setUserData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+      setUserData([]);
+    }
+  };
+
   return (
     <div>
       <div id="search">
-        <input id="searchInput" />
-        <img id="SearchTap" src={SearchTap} alt="" />
+        <input
+          id="searchInput"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <img
+          id="SearchTap"
+          src={SearchTap}
+          alt="Search"
+          onClick={handleSearch}
+        />
+      </div>
+      <div>
+        {userData.map((user, index) => (
+          <div key={index}>
+            <img src={user.profile_img} alt={user.name} />
+            <p>{user.name}</p>
+            <p>{user.bio}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
