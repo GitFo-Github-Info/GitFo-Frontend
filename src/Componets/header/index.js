27199acbe1d4b885e02eb27b0aec6ca9b8135602
youngeunkim <pkhj286@gmail.com>
2023-09-03 10:44:11 +0900

    chore : css

--- src/Componets/header/index.js
+++ src/Componets/header/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./index.css";
-import Logo from "../../assets/logo.png";
+  import React from "react";
+  import "./index.css";
+  import Logo from "../../assets/logo.png";
 
-function Header() {
-  return (
-    <div>
-      <img id="logo" src={Logo} alt="logo" />
-      <div id="hr" />
-    </div>
-  );
-}
+  function Header() {
+    return (
+      <div>
+        <img id="logo" src={Logo} alt="logo" />
+        <div id="hr" />
+      </div>
+    );
+  }
 
-export default Header;
+  export default Header;
--- src/pages/userPage/index.js
+++ src/pages/userPage/index.js
@@ -37,41 +37,41 @@ function UserPage() {
   }
 
   return (
-    <div>
-    <Link to="/">Back to Search</Link>
-    <div id="userpage">
-      <img id="profile" src={userDetails.profile_img} alt={userDetails.name} />
-      <div id="infoBox">
-        <div id="info">
-          <div id="personal">
-            <p id="name">
-              {userDetails.name !== null ? userDetails.name : userDetails.id}
-            </p>
-            <p id="id">{userDetails.id}</p>
+    <div id="container">
+      <Link to="/" id="back">
+        Back to Search
+      </Link>
+      <div id="userpage">
+        <img id="profile" src={userDetails.profile_img} alt={userDetails.name} />
+        <div id="infoBox">
+          <div id="info">
+            <div id="personal">
+              <p id="name">{userDetails.name !== null ? userDetails.name : userDetails.id}</p>
+              <p id="id">{userDetails.id}</p>
+            </div>
+            <div id="bioBox">
+              <p id="bio">{userDetails.bio}</p>
+              <p id="repo">Repos : {userDetails.public_repos}</p>
+            </div>
           </div>
-          <div id="bioBox">
-            <p id="bio">{userDetails.bio}</p>
-            <p id="repo">Repos : {userDetails.public_repos}</p>
-          </div>
-        </div>
-        <div id="detailBox">
-          <div id="follow">
-            <p id="follower">{userDetails.followers}followers</p>
-            <p id="followings">{userDetails.following}following</p>
-          </div>
-          <div id="detail">
-            <p id="company">
-              <img id="companyImg" src={Company} alt="company" />
-              {userDetails.company}
-            </p>
-            <p id="location">
-              <img id="locationImg" src={Location} alt="location" />
-              {userDetails.location}
-            </p>
+          <div id="detailBox">
+            <div id="follow">
+              <p id="follower">{userDetails.followers}followers</p>
+              <p id="followings">{userDetails.following}following</p>
+            </div>
+            <div id="detail">
+              <p id="company">
+                <img id="companyImg" src={Company} alt="company" />
+                {userDetails.company}
+              </p>
+              <p id="location">
+                <img id="locationImg" src={Location} alt="location" />
+                {userDetails.location}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
